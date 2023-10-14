4c6a4f5214112928dcc692dcf482d2bf1dbef942
WarunaLab <waruna.lankathilaka@gmail.com>
2023-10-14 21:51:11 +0530

    js file modified in to rough functions

---
+++ index.js
@@ -0,0 +1,34 @@
+//This is the script file for project rock paper scissor
+/* Planning
+1. need to create to get user input. convert it to lower case, and save.
+2. function should ask to repeatedly
+3. when score is 5 end the program saying who won
+4. when ever user enter a choice, computer should randomly enter a choice also
+5. match choices and give a score 
+6. hierarchial score system should create
+7. count scores plater who gets 5 first win
+8. promt the win massege.
+9. function= getComputerChoice
+10. scoring system?
+*/
+// Opening..
+alert("Welcome to Rock Paper Scissor. Do you want to play?");
+
+//promt for play
+
+let playerChoice = prompt('Enter your weapon ( Rock, Paper, Scissor)').toLowerCase;
+
+console.log(playerChoice);
+
+//function for play
+let playRound = (playerChoice, computerChoice) => {
+
+
+}
+
+//code for to alert who won in each round
+alert(`You lose ${} beat ${}`)
+
+//final game function
+
+    
